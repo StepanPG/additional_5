4e93479b2984e25f2706df0c3927be4ac01ad515
StepanPG <stepan-mandarin@yandex.ru>
2017-09-29 17:55:49 +0300

    check implementation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let s = [],
+    openingBrackets = bracketsConfig.map((el) => {
+      return el[0]
+    }),
+    closingBrackets = bracketsConfig.map((el) => {
+      return el[1]
+    });
+
+  for (let i = 0; i < str.length; i++) {
+
+    for (let x = 0; x < bracketsConfig.length; x++) {
+
+      if (str[i] == s[s.length - 1] && s[s.length - 1] == openingBrackets[x] && openingBrackets[x] == closingBrackets[x]) {
+        s.pop();
+      } else if (str[i] == openingBrackets[x]) {
+        s.push(str[i]);
+      } else if (str[i] == closingBrackets[x]) {
+        let brackets = s.pop() + str[i];
+        if (brackets != (openingBrackets[x] + closingBrackets[x])) {
+          return false;
+        }
+      }
+
+    }
+
+  }
+
+  if (!s.length) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
